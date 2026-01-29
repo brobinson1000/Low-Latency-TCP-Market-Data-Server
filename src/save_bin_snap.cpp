03919e3b92816c362dc40e7d2ae3f4934fd816f4
Brandon Robinson <1802brad@gmail.com>
2026-01-29 16:50:18 -0500

    fix error and log in save_bin

--- src/save_bin_snap.cpp
+++ src/save_bin_snap.cpp
@@ -1,12 +1,22 @@
 #include "save_bin_snap.h"
+#include "log_f.h"
 #include <fstream>
+#include <iostream>
+
+
 
 void save_bin_snap(const std::vector<Order>& orderbook, const std::string& filepath) {
     std::ofstream out(filepath, std::ios::binary | std::ios::trunc);
+
+    if(!out) {
+        std::cerr << "Saved file not found";
+    };
+
 
     uint64_t count = orderbook.size();
     out.write(reinterpret_cast<const char*>(&count), sizeof(count));
     out.write(reinterpret_cast<const char*>(orderbook.data()), count * sizeof(Order));
     out.close();
+    log_f("File closed and saved successfully");
 }
 
