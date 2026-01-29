c98f332364023f2e2550c84da3021bfcebca2e4c
Brandon Robinson <1802brad@gmail.com>
2026-01-29 16:47:29 -0500

    add log for periodic save

--- src/periodic_save.cpp
+++ src/periodic_save.cpp
@@ -3,11 +3,12 @@
 #include <string>
 #include <iostream>
 #include "periodic_save.h"
+#include "log_f.h"
 
 void periodic_save(std::vector<Order>& orderbook, const std::string &filepath, int interval_minutes) {
     while (true) {
         std::this_thread::sleep_for(std::chrono::minutes(interval_minutes));
         save_bin_snap(orderbook, filepath);
-        std::cout << "Orderbook snapshot saved \n";
+        log_f("Orderbook snapshot saved");
     }
 };
