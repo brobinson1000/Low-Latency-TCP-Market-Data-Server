dac4df3ffb7c53416b8753b1c0a627aae87cbf48
Brandon Robinson <1802brad@gmail.com>
2026-01-29 16:46:17 -0500

    add logging for resolve_bid

--- src/resolve_bid.cpp
+++ src/resolve_bid.cpp
@@ -1,10 +1,12 @@
 #include "resolve_bid.h"
+#include "log_f.h"
 #include "OrderBook.h"
 
 void resolve_bid(int id, bool win) {
     for (auto& bet : orderbook) {
         if (bet.orderid == id) {
             bet.active = false;
+            log_f("Bid Resolved Successfully");
         }
     }
 }
